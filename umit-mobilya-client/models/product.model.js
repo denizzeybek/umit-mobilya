296cf0f43edb807bf031a1b39721feecdf62ee38
Deniz Zeybek <46136181+denizzeybek@users.noreply.github.com>
2024-11-27 02:47:51 +0300

    add login, register apis

--- umit-mobilya-client/models/product.model.js
+++ umit-mobilya-client/models/product.model.js
@@ -1,5 +1,3 @@
-// models/Product.js
-
 const mongoose = require('mongoose');
 
 // Ürün şeması
--- umit-mobilya-client/routes/index.js
+++ umit-mobilya-client/routes/index.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const productRoutes = require('./product.route');  // Ürün API rotalarını içe aktar
+const authRoutes = require('./auth.route');  // Ürün API rotalarını içe aktar
 
 // Ürün API'lerini yönlendir
 router.use('/api/products', productRoutes);
+router.use('/api/auth', authRoutes);
 
 module.exports = router;
