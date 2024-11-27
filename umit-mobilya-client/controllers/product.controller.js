70b9b386b5526a2ca8928bec23ca87fb9f4e98ae
Deniz Zeybek <46136181+denizzeybek@users.noreply.github.com>
2024-11-27 12:34:53 +0300

    update delete product function

--- umit-mobilya-client/controllers/product.controller.js
+++ umit-mobilya-client/controllers/product.controller.js
@@ -2,8 +2,7 @@
 
 const Product = require('../models/product.model');
 
-// Tüm ürünleri listeleme
-exports.getAllProducts = async (req, res) => {
+const getProducts = async () => {
   try {
     const products = await Product.find().populate('modules.productId');
     const newProduct = products.map((product) => {
@@ -30,9 +29,17 @@ exports.getAllProducts = async (req, res) => {
             category: module.productId.category,
           };
         }),
-      }
-    })
-    res.json(newProduct);
+      };
+    });
+    return newProduct;
+  } catch (error) {}
+};
+
+// Tüm ürünleri listeleme
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await getProducts();
+    res.status(201).json(products);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -66,7 +73,6 @@ exports.createProduct = async (req, res) => {
 
   try {
     const newProduct = await product.save();
-
     res.status(201).json(newProduct);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -76,10 +82,28 @@ exports.createProduct = async (req, res) => {
 // Ürün silme
 exports.deleteProduct = async (req, res) => {
   try {
-    const product = await Product.findByIdAndDelete(req.params.id);
+    const allProducts = await getProducts();
+
+    // Tüm ürünleri kontrol etme
+    for (const product of allProducts) {
+      for (const prd of product.modules || []) {
+        if (prd._id.toString() === req.params.id) {
+          return res.status(500).json({
+            message: `Bu ürün ${product.name} içerisinde kullanıldığı için silinemez`,
+          });
+        }
+      }
+    }
+
+    // Ürünü bulma
+    const product = await Product.findById(req.params.id);
     if (!product) {
       return res.status(404).json({ message: 'Ürün bulunamadı' });
     }
+
+    // Burada silme işlemini gerçekleştirebilirsiniz
+    await product.remove(); // veya product.deleteOne() kullanabilirsiniz.
+
     res.json({ message: 'Ürün silindi' });
   } catch (error) {
     res.status(500).json({ message: error.message });
