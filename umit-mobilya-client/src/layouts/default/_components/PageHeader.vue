d1f5f79ab7f58a17bfe928c83c6f6f08a98d2a05
Deniz Zeybek <46136181+denizzeybek@users.noreply.github.com>
2024-12-06 16:04:18 +0300

    update pageheader

--- umit-mobilya-client/src/layouts/default/_components/PageHeader.vue
+++ umit-mobilya-client/src/layouts/default/_components/PageHeader.vue
@@ -82,87 +82,87 @@ defineEmits<IEmits>();
 
 const items = computed(() => {
   return [
-    {
-      label: 'Company',
-      root: true,
-      items: [
-        [
-          {
-            items: [
-              {
-                label: 'Features',
-                icon: 'pi pi-list',
-                subtext: 'Subtext of item',
-              },
-              {
-                label: 'Customers',
-                icon: 'pi pi-users',
-                subtext: 'Subtext of item',
-              },
-              {
-                label: 'Case Studies',
-                icon: 'pi pi-file',
-                subtext: 'Subtext of item',
-              },
-            ],
-          },
-        ],
-        [
-          {
-            items: [
-              {
-                label: 'Solutions',
-                icon: 'pi pi-shield',
-                subtext: 'Subtext of item',
-              },
-              {
-                label: 'Faq',
-                icon: 'pi pi-question',
-                subtext: 'Subtext of item',
-              },
-              {
-                label: 'Library',
-                icon: 'pi pi-search',
-                subtext: 'Subtext of item',
-              },
-            ],
-          },
-        ],
-        [
-          {
-            items: [
-              {
-                label: 'Community',
-                icon: 'pi pi-comments',
-                subtext: 'Subtext of item',
-              },
-              {
-                label: 'Rewards',
-                icon: 'pi pi-star',
-                subtext: 'Subtext of item',
-              },
-              {
-                label: 'Investors',
-                icon: 'pi pi-globe',
-                subtext: 'Subtext of item',
-              },
-            ],
-          },
-        ],
-        [
-          {
-            items: [
-              {
-                image:
-                  'https://primefaces.org/cdn/primevue/images/uikit/uikit-system.png',
-                label: 'GET STARTED',
-                subtext: 'Build spectacular apps in no time.',
-              },
-            ],
-          },
-        ],
-      ],
-    },
+    // {
+    //   label: 'Company',
+    //   root: true,
+    //   items: [
+    //     [
+    //       {
+    //         items: [
+    //           {
+    //             label: 'Features',
+    //             icon: 'pi pi-list',
+    //             subtext: 'Subtext of item',
+    //           },
+    //           {
+    //             label: 'Customers',
+    //             icon: 'pi pi-users',
+    //             subtext: 'Subtext of item',
+    //           },
+    //           {
+    //             label: 'Case Studies',
+    //             icon: 'pi pi-file',
+    //             subtext: 'Subtext of item',
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //     [
+    //       {
+    //         items: [
+    //           {
+    //             label: 'Solutions',
+    //             icon: 'pi pi-shield',
+    //             subtext: 'Subtext of item',
+    //           },
+    //           {
+    //             label: 'Faq',
+    //             icon: 'pi pi-question',
+    //             subtext: 'Subtext of item',
+    //           },
+    //           {
+    //             label: 'Library',
+    //             icon: 'pi pi-search',
+    //             subtext: 'Subtext of item',
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //     [
+    //       {
+    //         items: [
+    //           {
+    //             label: 'Community',
+    //             icon: 'pi pi-comments',
+    //             subtext: 'Subtext of item',
+    //           },
+    //           {
+    //             label: 'Rewards',
+    //             icon: 'pi pi-star',
+    //             subtext: 'Subtext of item',
+    //           },
+    //           {
+    //             label: 'Investors',
+    //             icon: 'pi pi-globe',
+    //             subtext: 'Subtext of item',
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //     [
+    //       {
+    //         items: [
+    //           {
+    //             image:
+    //               'https://primefaces.org/cdn/primevue/images/uikit/uikit-system.png',
+    //             label: 'GET STARTED',
+    //             subtext: 'Build spectacular apps in no time.',
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   ],
+    // },
     {
       label: 'Products',
       route: { name: ERouteNames.ProductsList },
